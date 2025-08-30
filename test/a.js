70ca3bf661b957c981eaafd354ab7ebe6b40c05d
FlylerSky <roesuryvoe@gmail.com>
2025-08-30 19:02:33 +0700

    update

---
+++ test/a.js
@@ -0,0 +1,14 @@
+console.log("Đã kết nối...");
+
+a = confirm("Bạn có NY chưa?");
+console.log(a);
+
+if (a) {
+ alert("Thật tuyệt vời");
+} else {
+ alert("Ô vẫn còn ế hả?");
+};
+
+for (var prop in object) {
+ // Tab to edit
+}
